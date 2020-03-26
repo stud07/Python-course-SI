ab50836846a6c84bbc8c45d2bb71a73e243e7e48
stud07 <majkunia7@gmail.com>
2020-03-26 17:54:17 +0100

    lab_2

--- lab_2.py
+++ lab_2.py
@@ -1,4 +1,5 @@
 from lab1 import bubble_sort, quick_sort
+import time
 
 def onsets(list,function,threshold):
     counter = 3
@@ -20,9 +21,21 @@ def onsets(list,function,threshold):
         print(list.index(numbers[j]))
 
 
-
+lista = [3,6,2,9,5,6]
 onsets([3,6,2,9,5,6],bubble_sort,3)
 onsets([3,6,2,9,5,6],quick_sort,3)
 
+test_list = []
+for it in range(100000):
+    test_list.append(it)
 
 
+
+t = time.time()
+print(onsets(test_list,bubble_sort,10000))
+print(time.time()-t)
+
+
+t = time.time()
+print(onsets(test_list,quick_sort,10000))
+print(time.time()-t)
